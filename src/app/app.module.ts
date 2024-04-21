ee8d112bb1a16070cb7f682a1f73379418cdd0d3
Ttale NOBLIA <ttale.noblia@soprahr.com>
2024-04-21 15:37:21 +0200

    change structure, create home page, fix curtain links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarteRecetteComponent } from './carte-scroll/carte-recette/carte-recette.component';
-import { CarteScrollComponent } from './carte-scroll/carte-scroll.component';
+import { CarteRecetteComponent } from './home-page/carte-scroll/carte-recette/carte-recette.component';
+import { CarteScrollComponent } from './home-page/carte-scroll/carte-scroll.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactInfoComponent } from './footer/contact-info/contact-info.component';
-import { AccueilComponent } from './accueil/accueil.component';
+import { AccueilComponent } from './home-page/accueil/accueil.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     ContactInfoComponent,
     AccueilComponent,
     NavbarComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
