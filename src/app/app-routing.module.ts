db96d9ca1d0167436291bc51b273499dfd87c305
Ttale NOBLIA <ttale.noblia@soprahr.com>
2024-04-21 15:48:32 +0200

    add and set router component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  // redirect to '/home'
+  { path: 'home', component: HomePageComponent },  // Home page route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
